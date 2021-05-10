4d9fa9de9898f35a64a3e497de0c3b5d8bfec9c8
atifghaffar83 <atifghaffar83@gmail.com>
2021-05-10 14:23:19 +0100

    Final

--- StockBuySell/Program.cs
+++ StockBuySell/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.IO;
+using System.Collections.Generic;
 
 namespace StockBuySell
 {
@@ -7,13 +9,30 @@ namespace StockBuySell
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Finding Highest Opening Balance and Lower Opening Balance from provided list");
-            double[] openingBalancesList = { 19.15, 18.30, 18.88, 17.93, 15.95, 19.03, 19.00, 20.00, 16.75, 25.19, 13.49 };
-            
-            for (int i=0; i < openingBalancesList.Length; i++)
-            {
-                Console.Write(openingBalancesList[i]+ ", ");
-            }
+            //-----------------------------------------------------------
+            //reading csv file
+            /* 
+             List<string> readFile = new List<string>();
+             using (var reader = new StreamReader(@"dataset.csv"))
+             {
+                 while (!reader.EndOfStream)
+                 {
+                     var line = reader.ReadLine();
+                     var values = line.Split(';');
+                     readFile.Add(values[0]);
+
+                 }
+
+             }
+
+             List<double> openingBalancesList = new List<double>();
+             double temp;
+             openingBalancesList = readFile.Select(x => double.TryParse(x, out temp) ? temp : 0).ToList();
+             */
+
+            //-----------------------------------------------------------
+            //we can use the following dataset if we not uploading the csv file
+            double[] openingBalancesList = { 19.55, 19.15, 16.15, 18.35, 17.38, 18.39, 18.88, 17.87, 17.77, 17.37, 18.39, 18.88, 17.87, 19.15, 16.15, 18.39, 17.39, 18.39, 17.87, 17.77, 17.39, 18.37, 18.88, 19.15, 16.15, 18.35, 16.13, 19.15, 16.15, 18.39 };
 
             //minimum stock and day value
             double minStockValue = openingBalancesList.Min();
@@ -25,7 +44,9 @@ namespace StockBuySell
 
             //output display on console
             Console.WriteLine($"\n{minStockValueDay}({minStockValue}), {maxStockValueDay}({maxStockValue})");
+            
 
+            
         }
     }
 }
